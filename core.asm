6fc768c48f73bfa1ff5516eeb308f1de31c3c91d
Sergei2000 <dsdem@mail.ru>
2020-11-25 14:37:59 +0300

    enabled protected mode

--- core.asm
+++ core.asm
@@ -1,19 +1,16 @@
 use16
+org 1000h
+
 
 xor ax,ax;
-mov ax,0x100;
+mov ax,0x10;
 mov ds,ax;
-lock mov byte[ds:00],1; меняю область памяти для проверки передачи управления
+;mov byte[ds:00],1; меняю область памяти для проверки передачи управления
 
 
-xor eax,eax;
-mov ax,cs;
-shl eax,4;
-add eax,gdt;
-mov  [gdt_reg.adresss],eax;
 
 xor eax,eax;
-mov ax,1000h
+mov ax,100h
 mov ds, ax;
 xor eax,eax;
 mov esi,eax;
@@ -27,44 +24,46 @@ mov es,ax;
 xor ax,ax;
 mov di,ax;
 
-xor cx,cx;
+xor ecx,ecx;
 mov cl,byte[code_length];
 
 rep movsb;
-
-
 
 cli;
 xor ax,ax
 mov ds,ax;
 
-lgdt fword[gdt_reg];
+lgdt [gdt_reg];
 
 
 mov eax,cr0;
 or al,1;
 mov cr0,eax; enable protected mode
 
-jmp  fword 8:20000h;
+jmp  8:moy_cod;
 
 moy_cod:
 use32
+
+mov ax,16;
+mov es,ax;
+mov ds,ax;
+mov byte[ds:0x100],1;
+
 jmp $;
+
 moy_cod_end:
 
 code_length db moy_cod_end-moy_cod; 
 
-struc gdt_registr razmer,adresss
-{
-  .razmer dw razmer
-  .adresss dd adresss
-}
 
-gdt_reg gdt_registr 23,0
 align 8
 gdt: 
 zero db 0,0,0,0,0,0,0,0;
 code db 0xff,0xff,0,0,0,10011010b,11001111b,0; pamyat dlya coda v 32 mode
 data_seg db 0xff, 0xff, 0, 0, 0,10010110b,11001111b,0; pamyat dlya dannyh v 32 mode 
-
+gdtend:
+gdt_reg:
+	.size dw gdtend-gdt-1;
+	.address dd gdt;
 
